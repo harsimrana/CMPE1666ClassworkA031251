c42fb9ae8d9eae48d966031daa842646dd2a24d2
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-09-10 14:47:24 -0600

    Demo 2 Created

---
+++ Demo02w2day2Sep10_controls/Demo02w2day2Sep10_controls/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Demo02w2day2Sep10_controls
+{
+    partial class Demo02
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_Display_Lbl = new System.Windows.Forms.Label();
+            this.UI_Start_Btn = new System.Windows.Forms.Button();
+            this.UI_Input_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Transfer_Btn = new System.Windows.Forms.Button();
+            this.UI_Input1_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Input2_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Sum_Btn = new System.Windows.Forms.Button();
+            this.UI_Screen_Lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_Display_Lbl
+            // 
+            this.UI_Display_Lbl.AutoSize = true;
+            this.UI_Display_Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UI_Display_Lbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.UI_Display_Lbl.Location = new System.Drawing.Point(12, 9);
+            this.UI_Display_Lbl.MinimumSize = new System.Drawing.Size(300, 20);
+            this.UI_Display_Lbl.Name = "UI_Display_Lbl";
+            this.UI_Display_Lbl.Padding = new System.Windows.Forms.Padding(20);
+            this.UI_Display_Lbl.Size = new System.Drawing.Size(300, 61);
+            this.UI_Display_Lbl.TabIndex = 0;
+            this.UI_Display_Lbl.Text = "0";
+            // 
+            // UI_Start_Btn
+            // 
+            this.UI_Start_Btn.Location = new System.Drawing.Point(12, 95);
+            this.UI_Start_Btn.Name = "UI_Start_Btn";
+            this.UI_Start_Btn.Size = new System.Drawing.Size(300, 58);
+            this.UI_Start_Btn.TabIndex = 1;
+            this.UI_Start_Btn.Text = "Start";
+            this.UI_Start_Btn.UseVisualStyleBackColor = true;
+            this.UI_Start_Btn.Click += new System.EventHandler(this.UI_Start_Btn_Click);
+            // 
+            // UI_Input_Tbx
+            // 
+            this.UI_Input_Tbx.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.UI_Input_Tbx.Location = new System.Drawing.Point(343, 9);
+            this.UI_Input_Tbx.MinimumSize = new System.Drawing.Size(300, 20);
+            this.UI_Input_Tbx.Multiline = true;
+            this.UI_Input_Tbx.Name = "UI_Input_Tbx";
+            this.UI_Input_Tbx.Size = new System.Drawing.Size(300, 61);
+            this.UI_Input_Tbx.TabIndex = 2;
+            // 
+            // UI_Transfer_Btn
+            // 
+            this.UI_Transfer_Btn.BackColor = System.Drawing.Color.RosyBrown;
+            this.UI_Transfer_Btn.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.UI_Transfer_Btn.Location = new System.Drawing.Point(343, 95);
+            this.UI_Transfer_Btn.Name = "UI_Transfer_Btn";
+            this.UI_Transfer_Btn.Size = new System.Drawing.Size(300, 58);
+            this.UI_Transfer_Btn.TabIndex = 3;
+            this.UI_Transfer_Btn.Text = "Transfer Text";
+            this.UI_Transfer_Btn.UseVisualStyleBackColor = false;
+            this.UI_Transfer_Btn.Click += new System.EventHandler(this.UI_Transfer_Btn_Click);
+            // 
+            // UI_Input1_Tbx
+            // 
+            this.UI_Input1_Tbx.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.UI_Input1_Tbx.Location = new System.Drawing.Point(12, 206);
+            this.UI_Input1_Tbx.MinimumSize = new System.Drawing.Size(300, 20);
+            this.UI_Input1_Tbx.Multiline = true;
+            this.UI_Input1_Tbx.Name = "UI_Input1_Tbx";
+            this.UI_Input1_Tbx.Size = new System.Drawing.Size(300, 61);
+            this.UI_Input1_Tbx.TabIndex = 4;
+            // 
+            // UI_Input2_Tbx
+            // 
+            this.UI_Input2_Tbx.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.UI_Input2_Tbx.Location = new System.Drawing.Point(343, 206);
+            this.UI_Input2_Tbx.MinimumSize = new System.Drawing.Size(300, 20);
+            this.UI_Input2_Tbx.Multiline = true;
+            this.UI_Input2_Tbx.Name = "UI_Input2_Tbx";
+            this.UI_Input2_Tbx.Size = new System.Drawing.Size(300, 61);
+            this.UI_Input2_Tbx.TabIndex = 5;
+            // 
+            // UI_Sum_Btn
+            // 
+            this.UI_Sum_Btn.Location = new System.Drawing.Point(12, 293);
+            this.UI_Sum_Btn.Name = "UI_Sum_Btn";
+            this.UI_Sum_Btn.Size = new System.Drawing.Size(140, 58);
+            this.UI_Sum_Btn.TabIndex = 6;
+            this.UI_Sum_Btn.Text = "Sum";
+            this.UI_Sum_Btn.UseVisualStyleBackColor = true;
+            this.UI_Sum_Btn.Click += new System.EventHandler(this.UI_Sum_Btn_Click);
+            // 
+            // UI_Screen_Lbl
+            // 
+            this.UI_Screen_Lbl.AutoSize = true;
+            this.UI_Screen_Lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UI_Screen_Lbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.UI_Screen_Lbl.Location = new System.Drawing.Point(12, 380);
+            this.UI_Screen_Lbl.MinimumSize = new System.Drawing.Size(650, 20);
+            this.UI_Screen_Lbl.Name = "UI_Screen_Lbl";
+            this.UI_Screen_Lbl.Padding = new System.Windows.Forms.Padding(20);
+            this.UI_Screen_Lbl.Size = new System.Drawing.Size(650, 61);
+            this.UI_Screen_Lbl.TabIndex = 7;
+            this.UI_Screen_Lbl.Text = "0";
+            // 
+            // Demo02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.UI_Screen_Lbl);
+            this.Controls.Add(this.UI_Sum_Btn);
+            this.Controls.Add(this.UI_Input2_Tbx);
+            this.Controls.Add(this.UI_Input1_Tbx);
+            this.Controls.Add(this.UI_Transfer_Btn);
+            this.Controls.Add(this.UI_Input_Tbx);
+            this.Controls.Add(this.UI_Start_Btn);
+            this.Controls.Add(this.UI_Display_Lbl);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Demo02";
+            this.Text = "Demo 02";
+            this.Load += new System.EventHandler(this.Demo02_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UI_Display_Lbl;
+        private System.Windows.Forms.Button UI_Start_Btn;
+        private System.Windows.Forms.TextBox UI_Input_Tbx;
+        private System.Windows.Forms.Button UI_Transfer_Btn;
+        private System.Windows.Forms.TextBox UI_Input1_Tbx;
+        private System.Windows.Forms.TextBox UI_Input2_Tbx;
+        private System.Windows.Forms.Button UI_Sum_Btn;
+        private System.Windows.Forms.Label UI_Screen_Lbl;
+    }
+}
+
---
+++ Demo02w2day2Sep10_controls/Demo02w2day2Sep10_controls/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo02w2day2Sep10_controls
+{
+    public partial class Demo02 : Form
+    {
+        int count = 0; // Variable count to keep track of number of clicks
+        public Demo02()
+        {
+            InitializeComponent();
+        }
+
+        private void Demo02_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Form is loaded");
+        }
+
+        private void UI_Start_Btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine(" Start button is clicked");
+            // Change the title of your form to something nice of your choice
+            // keyword this refers to current form 
+            this.Text = "Demo 02 Form";
+
+            if (count < 10)
+            {
+
+                System.Diagnostics.Trace.WriteLine($"{count}");
+                count++;
+                // string.Format() can perform complex formatting 
+                string displayMessage = string.Format(" Button is clicked {0} times", count);
+
+                // Version 2: String Interpolation
+                string displayMessage1 = $" Button is clicked {count} times";
+
+                // UI_Display_Lbl.Text = "Button is clicked "+ count.ToString() + " Times.";  //ToString() - to covert the value into a string
+
+                UI_Display_Lbl.Text = displayMessage1;
+            }
+            else
+            {
+                UI_Display_Lbl.ForeColor = Color.Red;
+                UI_Display_Lbl.Text = $" Button is clicked {count} times. You won't be able to do it now.";
+            }
+        }
+
+        private void UI_Transfer_Btn_Click(object sender, EventArgs e)
+        {
+            // Debugging part : Where, What event, Date
+            System.Diagnostics.Trace.WriteLine(" Transfer button is clicked " + DateTime.Now.ToString());
+
+            /*
+             *  Take the text from Text Box and show it on label 
+             *  Then, emply your text box
+             */
+
+            if (UI_Input_Tbx.Text.ToString().Length > 0)
+            {
+                UI_Input_Tbx.ForeColor = Color.White;
+                UI_Input_Tbx.BackColor = Color.Green;
+
+                UI_Display_Lbl.Text = UI_Input_Tbx.Text;  // Copying the text from textbox to label
+
+                UI_Input_Tbx.Text = String.Empty; // or "" empty string  Making text box empty
+
+            }
+            else {
+
+                // TO TEST THIS UNCOMMENT THE FOLLOWING
+                //UI_Input_Tbx.ForeColor = Color.White;
+                //UI_Input_Tbx.BackColor = Color.Red;
+                // Give a warning message
+                //UI_Input_Tbx.Text = "You cannot leave it empty";
+
+                // Give a pop up message for warning
+
+                // MessageBox is not a control, it is used to displayed error/warning messages to user
+                MessageBox.Show("You cannot leave text box empty");
+            }
+            
+            /*
+             * Make changes to this program:
+             * 1. Make sure Text box string should not be empty 
+             * 2. Try to change the background and foreground color [your choice] label and/or textbox 
+             * */
+        }
+
+        private void UI_Sum_Btn_Click(object sender, EventArgs e)
+        {
+            // Grab text from textbox 1, textbox 2
+            // Perform addition
+            // Show the result on screen label
+            int result = int.Parse(UI_Input1_Tbx.Text) + int.Parse(UI_Input2_Tbx.Text);
+
+            UI_Screen_Lbl.Text = result.ToString();
+
+        }
+    }
+}
