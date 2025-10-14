aa7de9e48e87eb6c319e5ef789161684fb63d80a
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-10-14 11:30:44 -0600

    Week 07 Day 1 Demo Drag and Drp
    Week 07 Day 1 Demo Drag and Drop
    14.10.2025 - Harsimranjot

---
+++ Week07Day01DemoDragDrop/Week07Day01DemoDragDrop/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Week07Day01DemoDragDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_Lbl_DragDrop = new System.Windows.Forms.Label();
+            this.UI_Lbx_FileData = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // UI_Lbl_DragDrop
+            // 
+            this.UI_Lbl_DragDrop.AllowDrop = true;
+            this.UI_Lbl_DragDrop.AutoSize = true;
+            this.UI_Lbl_DragDrop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Lbl_DragDrop.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.UI_Lbl_DragDrop.Location = new System.Drawing.Point(41, 26);
+            this.UI_Lbl_DragDrop.Name = "UI_Lbl_DragDrop";
+            this.UI_Lbl_DragDrop.Size = new System.Drawing.Size(189, 25);
+            this.UI_Lbl_DragDrop.TabIndex = 0;
+            this.UI_Lbl_DragDrop.Text = "Drop your file here";
+            this.UI_Lbl_DragDrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.UI_Lbl_DragDrop_DragDrop);
+            this.UI_Lbl_DragDrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.UI_Lbl_DragDrop_DragEnter);
+            // 
+            // UI_Lbx_FileData
+            // 
+            this.UI_Lbx_FileData.FormattingEnabled = true;
+            this.UI_Lbx_FileData.Location = new System.Drawing.Point(46, 77);
+            this.UI_Lbx_FileData.Name = "UI_Lbx_FileData";
+            this.UI_Lbx_FileData.Size = new System.Drawing.Size(184, 290);
+            this.UI_Lbx_FileData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_Lbx_FileData);
+            this.Controls.Add(this.UI_Lbl_DragDrop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UI_Lbl_DragDrop;
+        private System.Windows.Forms.ListBox UI_Lbx_FileData;
+    }
+}
+
---
+++ Week07Day01DemoDragDrop/Week07Day01DemoDragDrop/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Week07Day01DemoDragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Drag Enter event listener : To give visually impression to user that this control will accept drag and drop feature
+        private void UI_Lbl_DragDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent( DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void UI_Lbl_DragDrop_DragDrop(object sender, DragEventArgs e)
+        {
+            // try -catch block to handle it carefully
+
+            // Getting the path of the file
+            string filePath = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+            Console.WriteLine(filePath);
+
+            string[] fileData = File.ReadAllLines(filePath);
+
+            // Add data read from file into your list
+            foreach (string line in fileData)
+            {
+                UI_Lbx_FileData.Items.Add(line);
+            }
+
+        }
+    }
+}
