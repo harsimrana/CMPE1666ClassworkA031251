cc5e5c9ad00e3750586dc960bde4c9631665bbcd
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-10-15 14:29:56 -0600

    Week 07 Day 2 Work Selection Sort
    Week 07 Day 2 Work Selection Sort

---
+++ Week07d2Selection_Insertion_SortAlgo/Week07d2Selection_Insertion_SortAlgo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week07d2Selection_Insertion_SortAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // array to stores numbers
+            int[] numbers = { 56, 7, 4, 9, 21, 37, 17, 91 };
+
+            // Print array before sorting
+            Console.WriteLine("List before sorting");
+            PrintValues(numbers);
+
+            // Sort the list using 
+            SelectionSort(numbers);
+
+            // Print array after sorting
+            Console.WriteLine("List after sorting");
+            PrintValues(numbers);
+
+            Console.ReadKey();
+
+        }
+
+        // Print the array to console window
+        public static void PrintValues(int[] list)
+        {
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        //Method to perform selection sort
+        // Inputs:  An array to sort
+        // Return:  Nothing
+        public static void SelectionSort(int[] arr)
+        {
+            int numberOfElements = arr.Length;
+
+            for (int i = 0; i < numberOfElements - 1; ++i)
+            { // Number of passes 
+
+                int minElementIndex = i;
+
+                // Finding the smallest element's index from rest of the list
+                for (int j = i + 1; j < numberOfElements; ++j)
+                {
+                    if (arr[j] < arr[minElementIndex])
+                    {
+                        minElementIndex = j;
+                    }
+                }
+
+                //Swap the found minimum element
+                int temp = arr[minElementIndex];
+                arr[minElementIndex] = arr[i];
+                arr[i] = temp;
+
+                // Without using temp location
+                // UNCOMMENT THE FOLLOWING TO TEST IT WITH FOLLOWING LOGIC WITHOUT TEMP LOCATION
+                /*
+                arr[minElementIndex] = arr[minElementIndex] + arr[i];
+                arr[i] = arr[minElementIndex] - arr[i];
+                arr[minElementIndex] = arr[minElementIndex] - arr[i];
+                */
+            }
+
+        }
+    }
+}
---
+++ Week07d2Selection_Insertion_SortAlgo/Week07d2Selection_Insertion_SortAlgo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Week07d2Selection_Insertion_SortAlgo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAIT")]
+[assembly: AssemblyProduct("Week07d2Selection_Insertion_SortAlgo")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d0f0907-fe79-42b5-909d-8e0031d14a70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
