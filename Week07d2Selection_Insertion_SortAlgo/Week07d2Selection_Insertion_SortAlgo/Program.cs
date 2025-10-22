f4b87a82974d2fa9c60fe3855af3c89a73749aea
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-10-22 14:52:30 -0600

    Insertion Sort Algo Done 22.10.2025

--- Week07d2Selection_Insertion_SortAlgo/Week07d2Selection_Insertion_SortAlgo/Program.cs
+++ Week07d2Selection_Insertion_SortAlgo/Week07d2Selection_Insertion_SortAlgo/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Week 08 Day 02: 22.10.2025
+// Adding Insertion sort part to extend it further
 namespace Week07d2Selection_Insertion_SortAlgo
 {
     internal class Program
@@ -11,19 +12,23 @@ namespace Week07d2Selection_Insertion_SortAlgo
         static void Main(string[] args)
         {
             // array to stores numbers
-            int[] numbers = { 56, 7, 4, 9, 21, 37, 17, 91 };
+            int[] numbers = { 1000, 7, 0, 9, 21, -37, 17, -1 };
 
             // Print array before sorting
             Console.WriteLine("List before sorting");
-            PrintValues(numbers);
+            PrintValues(numbers);  //n
 
             // Sort the list using 
-            SelectionSort(numbers);
+            //SelectionSort(numbers);
+
+            // Sort using insertsort
+            InsertionSort(numbers); //n2
 
             // Print array after sorting
             Console.WriteLine("List after sorting");
-            PrintValues(numbers);
+            PrintValues(numbers);  //n
 
+            // n2+ 2n  
             Console.ReadKey();
 
         }
@@ -36,6 +41,7 @@ namespace Week07d2Selection_Insertion_SortAlgo
                 Console.WriteLine(item);
             }
         }
+        // Week 07 
         //Method to perform selection sort
         // Inputs:  An array to sort
         // Return:  Nothing
@@ -71,6 +77,31 @@ namespace Week07d2Selection_Insertion_SortAlgo
                 */
             }
 
-        }
-    }
-}
+        }// end of function seletion sort
+
+        // Week 08 Day 02: 22.10.2025
+        // Method to perform insertion sort
+        // Inputs:  An array to sort
+        // Return:  Nothing
+        public static void InsertionSort(int[] arr)
+        {
+            //int[] numbers = { 56, 7, 4, 9, 21, 37, 17, 91 };
+
+            for (int i = 1; i < arr.Length ; ++i)
+            {
+                //Key: Element to be inserted
+                int temp = arr[i];
+                int j = i - 1;
+
+                // Move elements greater than key to one position ahead 
+                while (j >= 0 && temp < arr[j]  ) 
+                {   
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                // Insert the key at the correct position
+                arr[j +1] = temp;
+            }
+        }// end of function Insertion
+    }//End of Class
+}// End of namespace
