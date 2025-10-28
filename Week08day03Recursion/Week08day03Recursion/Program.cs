6226914341a9fa2faf890df3d022a8643cf5a55c
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-10-28 12:03:55 -0600

    Demo Linear and Binary Search
    Demo Linear [Iterative, Recursive] and Binary Search [Iterative only]

--- Week08day03Recursion/Week08day03Recursion/Program.cs
+++ Week08day03Recursion/Week08day03Recursion/Program.cs
@@ -32,8 +32,99 @@ namespace Week08day03Recursion
             // Factorial 5!= 5 x 4 x 3 x 2 x 1 = 120 
 
             Console.WriteLine($"5!=  { Factorial(5) } " );
+            Console.WriteLine($"Sum of numbers till {5} =  {CalculateTotal(5)} ");
+
+            // 1+ 2+ 3 + ...... + n = n * ( n + 1) / 2
+
+            // Declare an array for Linear search operation
+            int[] numbers = { 4, 5, 9, 15, 21, 37, 57, 91, 99};
+            int itemSeach = 100;
+/*
+            int indexOfItem = LinearSeachIterative(numbers, itemSeach);
+
+            if(indexOfItem ==-1)
+                Console.WriteLine($"{itemSeach}  is Not Found in the list");
+            else
+                Console.WriteLine($"{itemSeach}  is Found at index {indexOfItem}");
+*/
+/*
+            int indexOfItem = LinearSeachRecursive(numbers, itemSeach, 0);
+
+            if (indexOfItem == -1)
+                Console.WriteLine($"{itemSeach}  is Not Found in the list");
+            else
+                Console.WriteLine($"{itemSeach}  is Found at index {indexOfItem}");
+*/
+            int indexOfItem = BinarySeachIterative(numbers, itemSeach);
+
+            if (indexOfItem == -1)
+                Console.WriteLine($"{itemSeach}  is Not Found in the list");
+            else
+                Console.WriteLine($"{itemSeach}  is Found at index {indexOfItem}");
         }
 
+        private static int BinarySeachIterative(int[] collection, int target)
+        {
+            int low = 0, upper = collection.Length - 1, mid;
+
+            while ( low <= upper)
+            {
+                mid = (low + upper) / 2;
+
+                if (collection[mid] == target)
+                    return mid;
+                else if (target < collection[mid])
+                { // item is in left half of the list 
+                    upper = mid - 1;
+                }
+                else
+                { // item is in the right half of the list
+                    low = mid + 1;
+                }
+            }
+            return -1; // not in the list
+        }
+
+        private static int LinearSeachRecursive(int[] collection, int target, int index  = 0)
+        {
+            // Base Case 2: Element not foound (index out of bound)
+            // It is important to put case first: Index is going to be outside the upper bound for the final call
+            if (index >= collection.Length)
+                return -1;
+
+            // Base case 1: Element found
+            if (collection[index] == target)
+                return index;
+
+            return LinearSeachRecursive(collection, target, index + 1);
+           
+        }
+        private static int LinearSeachIterative(int[] collection, int target)
+        {
+            //int indexSearchItem = -1;  // -1 means not found
+
+            for (int i = 0; i < collection.Length; ++i)
+            {
+                if (collection[i] == target)
+                {
+                    return i;  // returning the index of item
+                }
+            }
+            // If you are here it means item is not found in the list
+            return -1; // -1 means not in list 
+        }
+
+        // Write a method to calculate total of first n numbers- Where N is provided by user
+
+        private static int CalculateTotal(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n + CalculateTotal(n - 1);
+        }
+         
         private static int Factorial(int n)
         {
             if (n == 0)  // Base Condition because 0! = 1
